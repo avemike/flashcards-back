1bb51b9e960078aed408221b4d7b76a8740875dc
avemike <fajny.michal.gajek@gmail.com>
2019-05-08 22:16:04 +0200

    flashcard model

---
+++ models/mFlashcard.js
@@ -0,0 +1,37 @@
+const Joi = require('joi');
+const mongoose = require('mongoose');
+
+const mFlashcard = mongoose.model('Flashcard', mongoose.Schema({
+    firstText: {
+        type: String,
+        required: true,
+        minlength: 1,
+        maxlength: 255
+    },
+    secondText: {
+        type: String,
+        required: true,
+        minlength: 1,
+        maxlength: 255
+    },
+    category: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Category'
+    },
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    }
+}));
+
+function validate(flashcard) {
+    const schema = {
+        firstText: Joi.string().min(1).max(255).required(),
+        secondText: Joi.string().min(1).max(255).required()
+    };
+
+    return Joi.validate(flashcard, schema);
+}
+
+exports.mFlashcard= mFlashcard;
+exports.validateFlashcard = validate;
