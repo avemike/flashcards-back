97ed110072f418b67e5afd9dfe53fca883b6c560
avemike <fajny.michal.gajek@gmail.com>
2019-05-08 23:14:39 +0200

    basic flashcards routes

---
+++ routes/flashcardsRoutes.js
@@ -0,0 +1,82 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const config = require('config');
+const router = express.Router();
+
+const { mFlashcard, validateFlashcard } = require('../models/mFlashcard')
+const { mFlashcardCategory, validateFlashcardCategory } = require('../models/mFlashcardCategory');
+
+// return all flashcards
+router.get('/', async (req, res) => {
+    
+    const flashcards = await mFlashcard.find().sort('firstText');
+
+    if (!flashcard) return res.status(404).send('Flashcards were not found.');    
+    else res.send(flashcards);
+    
+});
+
+// return flashcard with given id
+router.get('/:id', async (req, res) => {
+    
+    const flashcard = await mFlashcard.findOne({_id: mongoose.Types.ObjectId(req.params.id), userId: decoded._id}).sort('name');
+    
+    if (!flashcard) return res.status(404).send('The flashcard with the given ID was not found.');
+    else res.send(flashcard);
+});
+
+// creates flashcard
+router.post('/', async (req, res) => {
+    const { error } = validateFlashcard(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
+    
+    const flashcard = new mFlashcard({
+        firstText: req.body.firstText,
+        secondText: req.body.secondText,
+    });
+
+    flashcard = await flashcard.save();
+    
+    res.send(flashcard);
+});
+ 
+// change flashcard (by given id) properties
+router.put('/:id', async (req, res) => {
+    const { error } = validateFlashcard(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
+    
+    const flashcard = await mFlashcard.findOneAndUpdate({
+            _id: mongoose.Types.ObjectId(req.params.id)
+        }, {
+            firstText: req.body.firstText,
+            secondText: req.body.secondText
+        }, {
+            new: true
+        });
+
+    if (!flashcard) return res.status(404).send('The flashcard with given ID was not found.');
+
+    res.send(flashcard);
+});
+
+// delete flashcard by given id
+router.delete('/:id', async (req, res) => {
+    let flashcard = await mFlashcard.findOneAndDelete(
+        {
+            _id: mongoose.Types.ObjectId(req.params.id)
+        }
+    );
+    
+    // for the future 
+    // else while(await mFlashcardCategory.findOneAndDelete(
+    //     {
+    //         flashcardId: mongoose.Types.ObjectId(req.params.id)
+    //     }
+    // ));
+
+    if (!flashcard) return res.status(404).send('The flashcard with the given ID was not found.');    
+    res.send(flashcard);
+});
+
+
+module.exports = router;
