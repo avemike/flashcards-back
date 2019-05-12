bb87a25acc0cbbde65d44a1087e5880f481f0d2b
avemike <fajny.michal.gajek@gmail.com>
2019-05-12 15:05:36 +0200

    Added correct/bad answers to flashcard model

--- models/mFlashcard.js
+++ models/mFlashcard.js
@@ -14,6 +14,14 @@ const mFlashcard = mongoose.model('Flashcard', new mongoose.Schema({
         minlength: 1,
         maxlength: 255
     },
+    correctAnswers: {
+        type: Number,
+        default: 0
+    },
+    badAnswers: {
+        type: Number,
+        default: 0
+    },
     userId: {
         type: mongoose.Schema.Types.ObjectId,
         ref: 'User'
@@ -23,7 +31,9 @@ const mFlashcard = mongoose.model('Flashcard', new mongoose.Schema({
 function validate(flashcard) {
     const schema = {
         firstText: Joi.string().min(1).max(255).required(),
-        secondText: Joi.string().min(1).max(255).required()
+        secondText: Joi.string().min(1).max(255).required(),
+        correctAnswers: Joi.number(),
+        badAnswers: Joi.number()
     };
 
     return Joi.validate(flashcard, schema);
